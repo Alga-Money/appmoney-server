1aa390443870598afa6028cbf3a60da2bb2388f7
akuma <diogerafael@gmail.com>
2020-03-13 21:55:16 -0300

    Correcao de bug body null

--- app.js
+++ app.js
@@ -12,14 +12,16 @@ const app = express()
 app.use(logger('dev'))
 app.use(cors())
 
-require('./routers')(app)
-
 // app.use(servicesAuth.checkSession)
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.use(bodyParser.raw())
+// app.use(bodyParser.raw())
+
 app.use(helmet())
 app.use(cookieParser())
+
+require('./routers')(app)
+
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
